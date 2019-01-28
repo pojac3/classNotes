2b898c67d1d67977d7121f10ca042c2cf139fb4f
Jacob Courtney <jacobc@ou.edu>
2019-01-28 13:22:39 -0600

    Initial Commit

---
+++ classNotes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  classNotes
+//
+//  Created by Jacob Courtney on 1/28/19.
+//  Copyright © 2019 Jacob Courtney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
