f1e9b4e92392c6dd83cc48290ef661a18b934e7b
Jacob Courtney <jacobc@ou.edu>
2019-02-06 13:51:29 -0600

    Before Class Feb 6

--- classNotes/main.cpp
+++ classNotes/main.cpp
@@ -1,76 +1,69 @@
-//
-//  This class is used to take notes in my data structures class. The code
-//  seen here is only meant as a test and will be heavily commented
-//
-//  classNotes 1/28
-//
-
 #include <iostream>
 using namespace std;
 
+template <class DT>
+void mySwap (DT& x, DT& y) {
+    DT temp;
+    
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+template  <class DT>
 class Point {
-    public:
-        double _x;
-        double _y;
-        int* _jack;
-        Point (); //default or empty constructor
-        Point (double x, double y); //non default constructor
-        ~Point(); //destructor. Why write code in the desctructor
-        Point (const Point& anotherOne); //copy constructor
-        Point& operator = (const Point& anotherOne);
-        void display();
+public:
+    DT* _x;
+    DT* _y;
+    Point();
+    void display();
+    Point<DT> getAPoint();
+    ~Point();
 };
 
-Point::Point() {
-    _x = 0.0;
-    _y = 0.0;
-    _jack = new int(0);
+template<class DT>
+Point<DT>::~Point<DT>() {
+    delete _x;
+    _x = NULL;
+    delete _y;
+    _y = NULL;
 }
 
-Point::Point(double x, double y) {
-    _x = x;
-    _y = y;
+template<class DT>
+Point<DT>::Point() {
+    _x = NULL;
+    _y = NULL;
+};
+template <class DT>
+Point<DT> Point<DT>::getAPoint() {
+    Point<DT>* P = new Point<DT>();
+    return *P;
 }
 
-Point::~Point() {
-    cout << "The point (" << _x << ", " << _y << ") was destroyed" << endl;
+template<class DT>
+void Point<DT>::display() {
+    cout << "(" << _x << ", " << _y << ")" << endl;
+};
+
+Point<int>* p1 = new Point<int>();
+Point<double>* p2 = new Point<double>();
+
+int main() {
+    int k = 10;
+    int j = 20;
     
-    //THIS DELETE LINE IS VERY IMPORTANT. DEEP DELETE _jack THIS WILL BE ON MIDTERM
-    delete _jack;
-}
-
-Point::Point (const Point& anotherOne) {
-    _x = anotherOne._x;
-    _y = anotherOne._y;
-    _jack = new int (*(anotherOne._jack));
-}
-
-void Point::display() {
-    cout << "The point is (" << _x << ", " << _y << ")" << endl;
-}
-
-Point& Point::operator = (const Point& anotherOne) {
-    _x = anotherOne._x;
-    _y = anotherOne._y;
-    _jack = new int (*(anotherOne._jack));
-    return (*this);
-}
-
-
-int main(int argc, const char * argv[]) {    //Starting in chapter one of textbook!
-    Point one;
+    char a = 'A';
+    char b = 'B';
     
-    Point* two = new Point(3.1,4.0); //dynamically defined
+    mySwap<int>(j,k);
+    mySwap<char>(a,b);
     
-    Point* three = new Point(*two);
-    cout << "Hello World!!!" << endl; //prints Hello World!!!
-    one.display();
-    (*two).display(); // two->display();
-    (*three).display();
+    cout << j << " " << k << endl;
+    cout << a << " " << b << endl;
     
-    Point four = one;
+    (*p1)._x = new int(10);
+    (*p1)._y = new int(20);
+    (*p1).display();
     
-    delete three;
-    delete two;
     return 0;
 }
