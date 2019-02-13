2f6badd2bb7154adfddf77af8b867cd5dbdf445c
Jacob Courtney <jacobc@ou.edu>
2019-02-13 14:45:59 -0600

    Class End Feb 13

--- classNotes/main.cpp
+++ classNotes/main.cpp
@@ -1,23 +1,47 @@
-//Feb 11, Data Structures
-//Learn Big Oh Notation, Big Omega Notation, Big Theta Notation and Big Alpha Notation for counting operations
-//Every "=" is one operation, accessing arrays is one, add/subtract/divide/multiply is one
-//f(n) is of order  g(n)
-//100n^2 - 1000n + 10^6 <-- f(n) is "big theta" of f(n^2)
-//Make sure to know correct Big O vs Big Theta. Big Theta is the highest degree of a polynomial, not Big O like in math
-//Binary search is Big O of log of n
-
-//Efficiency: Do NOT recompute
-//How to write a String back
-//Merge sort is THE fastest sorting method by far
-
-//standard template library, stl for project 2
-
+//Class notes Feb 13, Data Structures
+//We are currently in chapter 3 of the textbook
+//
 #include <iostream>
 using namespace std;
 
+int x[100];
+int* y = new int[100];
+
+template <class DT>
+class ArrayClass {
+public:
+    AbstractArrayClass<DT>;
+protected:
+    DT* _myArray;
+    //ArrayException would contain ArrayOutOfBoundsException and ArrayOutOfMemoryException
+    int _size;
+    
+};
+
+template <class DT>
+ArrayClass<DT>& ArrayClass<DT>::operator= {
+    _myArray = AC.myArray;
+    delete[] _myArray;
+    _myArray = new DT[AC.sign()];
+    _size = AC.size();
+    for (int i = 0; i < _size; i++) {
+        
+    }
+};
+
 int main() {
-    cout << "ur mum gay" << endl;
     
-    return 0;
+    ArrayClass<int> x(100);
+    ArrayClass<int>* y = new ArrayClass<int>(5);
     
+    for (int i = 0; i < x.size; i++) {
+        x[i] = 0;
+    }
+    
+    
+    
+    
+    //must have get method, set (i,value), size and
 }
+
+//QUIZ QUESTION:
