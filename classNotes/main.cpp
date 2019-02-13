89fc011f1a4889c42aefad3346f8f547f1a0778b
Jacob Courtney <jacobc@ou.edu>
2019-02-13 13:24:54 -0600

    Before Class Feb 13

--- classNotes/main.cpp
+++ classNotes/main.cpp
@@ -1,1 +1,23 @@
-//QUIZ QUESTION: Big Oh notation, Big Alpha Notation, Big Omega Notation
+//Feb 11, Data Structures
+//Learn Big Oh Notation, Big Omega Notation, Big Theta Notation and Big Alpha Notation for counting operations
+//Every "=" is one operation, accessing arrays is one, add/subtract/divide/multiply is one
+//f(n) is of order  g(n)
+//100n^2 - 1000n + 10^6 <-- f(n) is "big theta" of f(n^2)
+//Make sure to know correct Big O vs Big Theta. Big Theta is the highest degree of a polynomial, not Big O like in math
+//Binary search is Big O of log of n
+
+//Efficiency: Do NOT recompute
+//How to write a String back
+//Merge sort is THE fastest sorting method by far
+
+//standard template library, stl for project 2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "ur mum gay" << endl;
+    
+    return 0;
+    
+}
