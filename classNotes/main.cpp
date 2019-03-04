35be2949ca5f69c6f19cb08e072e92e5291afeca
Jacob Courtney <jacobc@ou.edu>
2019-03-04 13:31:16 -0600

    Before Class March 4

--- classNotes/main.cpp
+++ classNotes/main.cpp
@@ -1,120 +1,49 @@
-//VERY IMPORTANT CLASS THAT TELLS U HOW TO DO A LINKED LIST
-/*
- * Point Implementation
- * Array Implementation
- *
- * Project Talk: you have to implement LinkedList. f(x) = 2x^5 - 3x^2 + 10x - 500
- * we will create a class called term that will store the terms of a polynomial. class term has a coefficient and an exponent variable
- * class polynomial has a linkedlist of terms
+/* 
+ * Class Notes Feb 27
+ * Chapter 1, 2, 3 and 4 will be on the exam
+ * 
+ * Other forms of LinkedList:
+ * DoublyLinkedList. These have a next and previous pointer. The last element in this is just null
+ * CircularLinkedList. The last element points to the first element in this one.
+ * GeneralizedList. Has a next pointer and a down pointer. Can be use to store a tree of data
+ * 
+ * S T A C K S  A N D  Q U E U E S
+ * The stack is meant to remember the recent past. Stacks are used for backtracking!! 
+ * Peak and pop. This is a last in, last out data structure. The last element in a stack is the of the stack. 
+ * Peak and push takes O(1) time. 
+ * Dont continuously push something to the stack, cause you will get a StackOverflowException
+ * Dont continuously pop something from the stack, cause you will get a StackUnderflowException
+ * 
+ * TEST QUESTION: Give me a LinkedList representation of a stack. Defined below
  */
-
 #include <iostream>
 using namespace std;
 
 template <class DT>
-class LinkedList {
+class ArrayStack {
 protected:
-    DT* _info;
-    LinkedList<DT>* _next;
+    int _top;
     int _size;
+    ArrayClass<DT>* myStack;
 public:
-    LinkedList();
-    LinkedList(DT& i, LinkedList<DT>* n);
-    virtual ~LinkedList();
-    DT& operator[](int pos);
+    ArrayStack();
+    ArrayStack(int somethingGoesHere);
+    virtual ~ArrayStack();
+    DT& pop();
+    void push(DT& x);
+    DT& peak();
     int size();
-    void add(LinkedList<DT>& newOne);
-    void add(DT& other);
-    void insertAt(int pos, DT& x);
-    void remove();
-    DT& infoAt(int pos);
     
 };
 
 template <class DT>
-LinkedList<DT>::LinkedList() {
-    _info = NULL;
-    _next = NULL;
+void ArrayStack::push(DT& x) {
+    if (_top == *s.size())
+}
+
+
+int main () {
+    ArrayStack<int>* myStack = new ArrayStack<int>(100);
+    (*myStack).push(100);
+    (*myStack).push(20);
 };
-
-template <class DT>
-LinkedList<DT>::LinkedList(DT& i, LinkedList<DT>* n) {
-    _info = i;
-    _next = n;
-};
-
-template <class DT>
-LinkedList<DT>::~LinkedList<DT>() {
-    if (_info == NULL){
-        return;
-    }
-    else {
-        delete _info;
-        delete _next;
-    }
-}
-
-template <class DT>
-int LinkedList<DT>::size() {
-    if (_info == NULL) return 0;
-    return (1+(*_next).size());
-};
-
-//ADD ADDS TO THE BEGINNING OF THE LINKEDLIST
-
-template <class DT>
-void LinkedList<DT>::add(LinkedList<DT>& newOne) {
-    
-};
-
-template <class DT>
-void LinkedList<DT>::add(DT& other) {
-    LinkedList<DT>* temp = new LinkedList<DT>(_info,_next);
-    _info = new DT(other);
-    _next = temp;
-};
-
-template <class DT>
-void LinkedList<DT>::insertAt(int pos, DT& x) {
-    if (pos == 0) {
-        add(x);
-    }
-    else {
-        (*_next).insertAt(pos-1,x);
-    }
-}
-
-template <class DT>
-void LinkedList<DT>::remove() {
-    if (_info != NULL) {
-        delete _info;
-        LinkedList<DT>* temp = _next;
-        _info = (*temp)._info;
-        _next = (*temp)._next;
-        (*temp)._info = NULL;
-        (*temp)._next = NULL;
-        delete temp;
-    }
-}
-
-template<class DT>
-DT& LinkedList<DT>::infoAt(int pos) {
-    if (pos == 0) {
-        return *_info;
-    }
-    else {
-        return (*_next).infoAt(pos-1);
-    }
-}
-
-template <class DT>
-DT& LinkedList<DT>::operator[](int pos) {
-    return infoAt(pos);
-}
-
-int main() {
-    LinkedList<int>* one = new LinkedList<int>();
-    LinkedList<int>* two = new LinkedList<int>();
-    
-    return 0;
-}
