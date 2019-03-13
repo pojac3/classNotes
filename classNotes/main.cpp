8a3aeaa1c12e9161f2f394a9297c8d50046a3c19
Jacob Courtney <jacobc@ou.edu>
2019-03-13 13:31:09 -0500

    Before Class March 13

--- classNotes/main.cpp
+++ classNotes/main.cpp
@@ -1,51 +1,33 @@
 /**
- * Stuff in class today will be on the exam on wednesday
- * Array Implementation
- * LinkedList implementation
- * pop(); and push(); function
- * might ask us to write a c++ class definition for a stack data structure
- * StackOverflowException and StackUnderflow exception
- * In a LinkedList implementation of a stack, pop(); is the same as remove and push(); is the same as add
- * Arithmetic Expression is the same as infix expression. in x+2, x and 2 are the operands and + is the operator
- * In arithmetic expression, we take the infix expression and convert it to a postfix expression. so like xy needs to be converted to x*y
- * In postfix evaluation, xsub2 is an operand. Go through each token and decide if its an operand or operator. add it to an operand or operator array
- * Postfix expression pseudocode:
- * s <- stack
- * while (there is more tokens) {
- *   A = getNextToken();
- *   if (A is an operator) {
- *      a1 = s.pop();
- *      a2 = s.pop();
- *      perform a1 A a2
- *      x = result of a1 A a2
- *      s.push(x);
- *   }
- * MAY ASK US TO WRITE AN ALGORITHM ON HOW TO CONVERT FROM INFIX TO POSTFIX AND VICE VERSA
- * First question will be on stacks. GO READ ABOUT STACKS. PROBABLY EXPRESSION EVALUATION. Running through the maze
- * Know C++ class def for a LinkedList, size, remove, add removeAt, destructor, etc
- * Know how to do [] operator on a LinkedList. Should return the BOX _info. Not a pointer to _info. 
- * Define any method that you use. 
- * Know array implementation of a LinkedList. 
- * Give C++ defintion for a Generalized List. Inherits LinkedList!
- * LinkedList might have a super node. These point to a whole bunch of nodes. 
- * Go through the quiz questions!!!
- * Binary search recursive and non recursive algorithm!
- * ArrayClass. How to initialize one.
- * Oh ya and know Big O Notation
- * Counting number of primitive operations
- * Might give a program and specify the number of errors, gotta find all the errors
- * Write legibly
+ * Class Notes March 11
+ * Project4 will not be an extension of Project3
+ * Stack is a LIFO data structure, Queue is a FIFO data structure
+ * dequeue is the same as pop, but it removes from the front instead of from the back
+ * enqueue is insertAt(x, size()), dequeue is remove(), peek is info()
+ * Look at the book for Queue! WILL BE ON THE EXAM!
+ * front = (front + 1) % n;
+ * rear = (rear + 1) % n;
+ * ^^^ n is passed in, the size of the array (line 6)
+ * simple bucket sort:
+ * -non comparison based sorting algorithm
+ * -we know the largest number
+ * -large numbers
+ * -duplicates
+ * -largest number is too big relative to the other numbers to be sorted, ie {1, 5, 872359} would be very bad
+ * -time complexity is O(n+m), where m is the largest number, and n is the number of numbers
+ * -space complexity is ridiculous! if n+m is 100, then it requires 100*n space!!!
+ * 
+ * Radix Sort:
+ * -another non comparison based sorting algorithm
+ * -largest number is known
+ * -uses the queue data structure
+ * -worst case scenario is (10 + n) with an array size of 10
+ * 
  */
 
 #include <iostream>
 using namespace std;
-int n = 50;
-int x = 0;
-int main() {
-    for (int i = 0; i < n; i++){ //3n+2
-        for (int j = 0; j < n; j++) { //n*(3n+2)
-            x = x + 1; //2*n*n
-        }
-    }
-        
+
+int main () {
+    
 }
